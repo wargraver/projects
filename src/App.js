86142c7ed850f58ab0c5dfd04a07d91386480cf8
Utkarsh <utkarshkrsingh2091@gmail.com>
2020-07-19 21:01:27 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state={
+      newvalue:"",
+      list:[]
+    }
+  }
+  additem(val){
+    if(val==="") return 
+    else{
+      const list2=[...this.state.list]
+      const newval={
+        id:Date.now(),
+        value:val,
+        done:false
+      }
+      list2.push(newval)
+      this.setState({
+        list:list2,
+        newvalue:""
+      })
+    }
+  }
+  updateitem(val){
+    this.setState({newvalue:val})
+  }
+  deleteitem(id){
+    const list2=[...this.state.list]
+    const list3=list2.filter((item)=>item.id!==id)
+    this.setState({
+      list:list3
+    })
+  }
+  render(){
+    return(
+       <div className="back">
+         <header className="head">ToDo List...</header>
+         <input className="task" 
+         type="text" 
+         placeholder="Add your new task"
+         required
+         onChange={e => this.updateitem(e.target.value)}
+         value={this.state.newvalue}
+         
+         />
+         <button className="Add"
+         onClick={() => this.additem(this.state.newvalue)}
+         disabled={!this.state.newvalue.length}
+         >ADD</button>
+         <div>
+           <ul >
+             {this.state.list.map((item)=>{
+               return(
+                <li  key={item.id} calssName="list">
+                <input className="inp" type="checkbox"/>
+               {item.value}
+               <button className="Add" onClick={()=>{
+                 const list2=[...this.state.list]
+                 const list3=list2.filter(item2=>item2.id!==item.id)
+                 this.setState({list:list3})
+               }}>Delete Task</button>
+               </li>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+               )
+             })}
+             </ul>
+         </div>
+       </div>
+    )
+  }
 }
 
-export default App;
+export default App
